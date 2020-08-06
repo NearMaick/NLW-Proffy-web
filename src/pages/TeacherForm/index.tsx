0ab20bd9065fc3470ba88345d2d068c07dc3e1e4
Maick Souza <maick_a_s@msn.com>
2020-08-06 12:58:33 -0300

    feat(web): Teaxtarea

--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -4,6 +4,8 @@ import PageHeader from '../../components/PageHeader';
 import Input from '../../components/Input';
 
 import warningIcon from '../../assets/images/icons/warning.svg';
+import Textarea from '../../components/Textarea';
+import Select from '../../components/Select';
 
 import './styles.css';
 
@@ -22,12 +24,13 @@ function TeacherForm () {
           <Input name="name" label="Nome completo" />
           <Input name="avatar" label="Avatar" />
           <Input name="whatsapp" label="Whatsapp" />
+          <Textarea name="bio" label="Biografia" />
         </fieldset>
 
         <fieldset>
           <legend>Sobre a aula</legend>
           
-          <Input name="subject" label="Matéria" />
+          <Select name="subject" label="Matéria" />
           <Input name="cost" label="Custo da sua hora por aula" />
           <Input name="whatsapp" label="Whatsapp" />
         </fieldset>
